e4a079778f6f0f79ba623c66c9ccd8eddb394b3c
Bob Jiang <dashaomai@gmail.com>
2017-12-31 19:32:09 +0800

    修改成连续处理三个文件

--- main.cpp
+++ main.cpp
@@ -16,41 +16,45 @@ const size_t BKDRHash(const char *str) {
 	return hash;
 }
 
+const string paths[] = {"百度买房语料", "网易新闻语料", "网易新闻语料20171122"};
+
+void merge(set<size_t> *sentences, const string in_path, const string out_path) {
+  cout << "输入文件：" << in_path << "，输出文件：" << out_path << endl;
+
+  ifstream in;
+  in.open(in_path, ios_base::in);
+
+  ofstream out;
+  out.open(out_path, ios_base::out);
+
+  string buffer;
+  while (getline(in, buffer)) {
+    const auto str = buffer.c_str();
+    const auto hash = BKDRHash(str);
+    if (sentences->insert(hash).second) {
+      out << str << endl;
+    }
+  }
+
+  out.close();
+  in.close();
+}
+
 int main(int argc, char** argv) {
-	auto *sentences = new set<const size_t >();
+	auto *sentences = new set<size_t>();
+  const string out_path = "所有去重.unique.txt";
 
 	Path path(false);
 	path.pushDirectory(".");
 	path.pushDirectory("assets");
-	path.setFileName("百度买房语料.sentence.txt");
 
-  string in_path(path.makeAbsolute().toString());
+  for (int i=0, m=sizeof(paths)/sizeof(string); i<m; i++) {
+    const string p = paths[i];
+    path.setFileName(p + ".grouped.txt");
+    const string in_path(path.makeAbsolute().toString());
 
-	ifstream in;
-	in.open(in_path, ios_base::in);
-
-	path.setFileName("百度买房语料.grouped.txt");
-	in_path = path.makeAbsolute().toString();
-
-	ofstream out;
-	out.open(in_path, ios_base::out);
-
-	string buffer;
-
-	while (getline(in, buffer)) {
-		// 计算字符串的 hash 码，以检查是不是已经存在过的字符串
-		const auto str = buffer.c_str();
-		const auto hash = BKDRHash(str);
-		if (sentences->insert(hash).second) {
-			// out.write(str, strlen(str));
-      out << str << endl;
-		}
-	}
-
-	out.close();
-	in.close();
-
-	delete sentences;
+    merge(sentences, in_path, out_path);
+  }
 
   return EXIT_SUCCESS;
 }
