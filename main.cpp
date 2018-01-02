151167cf8cae8f36905575a4d6b0eab6fdab4f86
Bob Jiang <dashaomai@qq.com>
2018-01-02 23:24:52 +0000

    变成多线程版本

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 ﻿#include "Poco/Path.h"
 #include "Poco/Timestamp.h"
+#include "Poco/Notification.h"
+#include "Poco/NotificationQueue.h"
+#include "Poco/ThreadPool.h"
+#include "Poco/Runnable.h"
 
 #include <iostream>
 #include <fstream>
@@ -8,6 +12,10 @@
 using namespace std;
 using Poco::Path;
 using Poco::Int64;
+using Poco::Notification;
+using Poco::NotificationQueue;
+using Poco::ThreadPool;
+using Poco::Runnable;
 
 inline const size_t BKDRHash(const char *str) {
 	size_t hash = 0;
@@ -23,30 +31,85 @@ const Int64 currentTime() {
 	return now.epochMicroseconds();
 }
 
+class LineNotification : public Notification {
+  public:
+    LineNotification(const string *line) : _line(line) {}
+
+    const string* getLine() const {
+      return _line;
+    }
+
+  private:
+    const string *_line;
+};
+
+class MergeWorker : public Runnable {
+  public:
+    MergeWorker(NotificationQueue *queue, set<size_t> *sentences) : _queue(queue), _sentences(sentences) {}
+
+    void run() {
+      // while((Notification *notification = _queue.waitDequeueNotification()) != NULL) {
+      cout << "进入线程：" << Poco::Thread::current()->name() << endl;
+
+      for (Notification *notification = _queue->waitDequeueNotification();
+          NULL != notification; notification = _queue->waitDequeueNotification()) {
+        LineNotification *pLN = (LineNotification*)notification;
+        if (NULL != pLN) {
+          const auto pChar = pLN->getLine()->c_str();
+          const auto hash = BKDRHash(pChar);
+          if (_sentences->insert(hash).second) {
+            cout << pChar << endl;
+          }
+        }
+
+        notification->release();
+      }
+    }
+
+  private:
+    NotificationQueue *_queue;
+    set<size_t> *_sentences;
+};
+
 const string paths[] = { "百度买房语料", "网易新闻语料", "网易新闻语料20171122", "一般词全集语料", "一般词全集语料1", "一般词全集语料2", "一般词全集语料3", "一般词全集语料4", "一般词全集语料5" };
 
-void merge(set<size_t> *sentences, const string &in_path, ofstream &out) {
+void merge(NotificationQueue *queue, set<size_t> *sentences, const string &in_path, ofstream &out) {
 	cout << "输入文件：" << in_path << endl;
 
 	ifstream in;
 	in.open(in_path, ios_base::in);
 
-	string buffer;
-	while (getline(in, buffer)) {
-		const auto str = buffer.c_str();
+	string *buffer = new string;
+	while (getline(in, *buffer)) {
+		/*const auto str = buffer.c_str();
 		const auto hash = BKDRHash(str);
 		if (sentences->insert(hash).second) {
 			out << str << endl;
-		}
+		}*/
+
+    queue->enqueueNotification(new LineNotification(buffer));
 	}
 
 	in.close();
+
+  while (!queue->empty()) {
+    cout << "Wait for merge: " << in_path << endl;
+    Poco::Thread::sleep(100);
+  }
 }
 
 int main(int argc, char** argv) {
 	const Int64 begin = currentTime();
-
 	auto *sentences = new set<size_t>();
+
+  NotificationQueue *queue = new NotificationQueue;
+
+  MergeWorker worker1(queue, sentences);
+  MergeWorker worker2(queue, sentences);
+
+  ThreadPool::defaultPool().start(worker1);
+  ThreadPool::defaultPool().start(worker2);
+
 	const string out_url = "所有去重.unique.txt";
 
 	Path path(false);
@@ -63,12 +126,19 @@ int main(int argc, char** argv) {
 		path.setFileName(p + ".grouped.txt");
 		const string in_path(path.makeAbsolute().toString());
 
-		merge(sentences, in_path, out);
+		merge(queue, sentences, in_path, out);
 	}
 
 	out.close();
 
 	delete sentences;
+
+  while (!queue->empty()) {
+    cout << "Wait for all merged." << endl;
+    Poco::Thread::sleep(100);
+  }
+
+  delete queue;
 
 	const Int64 end = currentTime();
 
