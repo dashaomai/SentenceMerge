2756ccd95532e2c87fd56009b0efcb1add41b9bd
Bob Jiang <dashaomai@qq.com>
2018-01-02 15:21:53 +0800

    代码格式化了

--- main.cpp
+++ main.cpp
@@ -23,56 +23,56 @@ const Int64 currentTime() {
 	return now.epochMicroseconds();
 }
 
-const string paths[] = {"百度买房语料", "网易新闻语料", "网易新闻语料20171122", "一般词全集语料", "一般词全集语料1", "一般词全集语料2", "一般词全集语料3", "一般词全集语料4", "一般词全集语料5"};
+const string paths[] = { "百度买房语料", "网易新闻语料", "网易新闻语料20171122", "一般词全集语料", "一般词全集语料1", "一般词全集语料2", "一般词全集语料3", "一般词全集语料4", "一般词全集语料5" };
 
 void merge(set<size_t> *sentences, const string &in_path, ofstream &out) {
-  cout << "输入文件：" << in_path << endl;
+	cout << "输入文件：" << in_path << endl;
 
-  ifstream in;
-  in.open(in_path, ios_base::in);
+	ifstream in;
+	in.open(in_path, ios_base::in);
 
-  string buffer;
-  while (getline(in, buffer)) {
-    const auto str = buffer.c_str();
-    const auto hash = BKDRHash(str);
-    if (sentences->insert(hash).second) {
-      out << str << endl;
-    }
-  }
+	string buffer;
+	while (getline(in, buffer)) {
+		const auto str = buffer.c_str();
+		const auto hash = BKDRHash(str);
+		if (sentences->insert(hash).second) {
+			out << str << endl;
+		}
+	}
 
-  in.close();
+	in.close();
 }
 
 int main(int argc, char** argv) {
-  const Int64 begin = currentTime();
+	const Int64 begin = currentTime();
 
 	auto *sentences = new set<size_t>();
-  const string out_url = "所有去重.unique.txt";
+	const string out_url = "所有去重.unique.txt";
 
 	Path path(false);
 	path.pushDirectory(".");
 	path.pushDirectory("assets");
-  path.setFileName(out_url);
+	path.setFileName(out_url);
 
-  const string out_path(path.makeAbsolute().toString());
-  ofstream out;
-  out.open(out_path, ios_base::out);
+	const string out_path(path.makeAbsolute().toString());
+	ofstream out;
+	out.open(out_path, ios_base::out);
 
-  for (int i=0, m=sizeof(paths)/sizeof(string); i<m; i++) {
-    const string p = paths[i];
-    path.setFileName(p + ".grouped.txt");
-    const string in_path(path.makeAbsolute().toString());
+	for (int i = 0, m = sizeof(paths) / sizeof(string); i < m; i++) {
+		const string p = paths[i];
+		path.setFileName(p + ".grouped.txt");
+		const string in_path(path.makeAbsolute().toString());
 
-    merge(sentences, in_path, out);
-  }
+		merge(sentences, in_path, out);
+	}
 
-  out.close();
+	out.close();
 
-  delete sentences;
+	delete sentences;
 
-  const Int64 end = currentTime();
+	const Int64 end = currentTime();
 
-  cout << "合并所有文件到：" << out_path << "，耗时：" << end - begin << " 毫秒。" << endl;
+	cout << "合并所有文件到：" << out_path << "，耗时：" << end - begin << " 毫秒。" << endl;
 
-  return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
